9fa9411eb0427db9b29f7c493a93e8f424de1226
John Yi <jhy17@pitt.edu>
2022-02-05 13:40:10 -0500

    ok

---
+++ homeworks/CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: /home/johnhajin21/ECE0302/assignments/homeworks
+# Build directory: /home/johnhajin21/ECE0302/assignments/homeworks
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+add_test(unittest "bag-tests")
+set_tests_properties(unittest PROPERTIES  _BACKTRACE_TRIPLES "/home/johnhajin21/ECE0302/assignments/homeworks/CMakeLists.txt;16;add_test;/home/johnhajin21/ECE0302/assignments/homeworks/CMakeLists.txt;0;")
--- homeworks/abstract_bag.hpp
+++ homeworks/abstract_bag.hpp
@@ -1,1 +1,26 @@
-hello
+#ifndef ABSTRACT_BAG_HPP
+#define ABSTRACT_BAG_HPP
+
+#include <iostream>
+template <typename T> 
+
+class AbstractBag
+{
+
+virtual std::size_t getCurrentSize() const = 0;
+
+  virtual bool isEmpty() const = 0;
+
+  virtual bool add(const T& entry) = 0;
+
+  virtual bool remove(const T& entry) = 0;
+
+  virtual void clear() = 0;
+
+  virtual std::size_t getFrequencyOf(const T& entry) const = 0;
+
+  virtual bool contains(const T& entry) const = 0;
+
+};
+
+#endif
