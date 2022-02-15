6d939b555abc7404bbfae048dc572b86d6b837ec
johnhajin21 <97571384+johnhajin21@users.noreply.github.com>
2022-02-15 18:59:52 -0500

    Update FindPalindrome_main.cpp

--- projects/project2/FindPalindrome_main.cpp
+++ projects/project2/FindPalindrome_main.cpp
@@ -3,31 +3,30 @@
 
 using namespace std;
 
-//left unchanged
-
 int main()
 {
-FindPalindrome f;
-string newString("a");
-cout << "\n******* Project 1 Main *********\n\n";
-for (int i=0; i<3; i++) {
-if(f.add(newString)) {
-cout << "Added the string :" << newString << endl;
-} else {
-cout << "ERROR: for some reason unable to add the string: << newString " << endl;
+	FindPalindrome f;
+	string newString("a");
+	cout << "\n******* Project 1 Main *********\n\n";
+	for (int i=0; i<3; i++) {
+		if(f.add(newString)) {
+			cout << "Added the string :" << newString << endl;
+		} else {
+			cout << "ERROR: for some reason unable to add the string: << newString " << endl;
+		}
+		newString += "a";
+	}
+	cout << "\nNumber of sentence palindromes are:  " << f.number() << endl;
+	cout << "\nThe sentence palindromes are:" << endl;
+	vector<vector<string>> vectorList = f.toVector();
+	for (int i=0; i<vectorList.size(); i++) {
+		for (int j=0; j<vectorList[i].size(); j++) {
+			cout << vectorList[i][j] << " ";
+		}
+		cout << endl;
+	}
+	cout << endl;
+	cout << "\n******* Project 1 Main *********\n\n";
+	return 0;
 }
-newString += "a";
-}
-cout << "\nNumber of sentence palindromes are:  " << f.number() << endl;
-cout << "\nThe sentence palindromes are:" << endl;
-vector<vector<string>> vectorList = f.toVector();
-for (long unsigned int i=0; i<vectorList.size(); i++) {
-for (long unsigned int j=0; j<vectorList[i].size(); j++) {
-cout << vectorList[i][j] << " ";
-}
-cout << endl;
-}
-cout << endl;
-cout << "\n******* Project 1 Main *********\n\n";
-return 0;
-}
+
